c94c6b2bfe6467c8318d427d82bde758f98c0da7
JoseMuñoz <oemy19@gmail.com>
2024-04-10 20:25:23 +0200

    Esto es todo

--- cypress/e2e/heroku.cy.js
+++ cypress/e2e/heroku.cy.js
@@ -7,4 +7,19 @@ describe('template spec', () => {
     cy.get('.fa').click()
     cy.get('.radius').log('You logged into a secure area!')
   })
+
+  it('test de login invalido', () => {
+    cy.visit('https://the-internet.herokuapp.com/')
+    cy.get(':nth-child(21) > a').click()
+    cy.get('#username').type("tomsmit")
+    cy.get('#password').type("SuperSecretPassword!")
+    
+})
+it('test de login valido', () => {
+  cy.visit('https://the-internet.herokuapp.com/')
+  cy.get(':nth-child(21) > a').click()
+  cy.get('#username').type("tomsmith")
+  cy.get('#password').type("SuperSecretPassword")
+
+})
 })
